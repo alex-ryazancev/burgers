0876c77ff9e26b7aa8b11e1c60b477bef0756d23
Alexey <alex-ryazancev@mail.ru>
2020-03-29 20:45:55 +0300

    creat accordion

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 ///////////////// burger menu /////////////////
 
-let open = document.querySelector('#open');
-let close = document.querySelector('#close');
-let burger = document.querySelector('#burger');
+const openMenu = document.querySelector('.hamburger-menu-link'),
+  closeMenu = document.querySelector('.hamburger-menu__close'),
+  burgerMenu = document.querySelector('.hamburger-menu');
 
-open.addEventListener('click', function (event) {
+openMenu.addEventListener('click', function (event) {
   event.preventDefault();
 });
 
-open.addEventListener('click', function () {
-  burger.style.display = 'flex';
+openMenu.addEventListener('click', function () {
+  burgerMenu.style.display = 'flex';
 })
 
-close.addEventListener('click', function () {
-  burger.style.display = 'none';
+closeMenu.addEventListener('click', function () {
+  burgerMenu.style.display = 'none';
 })
 
 ///////////////// horizontal accordion /////////////////
@@ -24,7 +24,7 @@ const menu = document.querySelector('.menu'),
 
 menu.addEventListener('click', e => {
   for (let i = 0; i < accoItemLength; i++) {
-    accoItem[i].classList.remove('menu-acco__item—active');
+    accoItem[i].classList.remove('menu-acco__item--active');
   }
 });
 
@@ -33,18 +33,96 @@ for (let i = 0; i < accoItemLength; i++) {
     e.stopPropagation();
     e.preventDefault();
 
-    if (accoItem[i].classList.contains('menu-acco__item—active')) 
-    {
-      accoItem[i].classList.remove('menu-acco__item—active');
+    if (accoItem[i].classList.contains('menu-acco__item--active')) {
+      accoItem[i].classList.remove('menu-acco__item--active');
     }
     else {
       for (let i = 0; i < accoItemLength; i++) {
-        accoItem[i].classList.remove('menu-acco__item—active');
+        accoItem[i].classList.remove('menu-acco__item--active');
       }
-      accoItem[i].classList.add('menu-acco__item—active');
+      accoItem[i].classList.add('menu-acco__item--active');
     }
-})
+  })
 }
 
-///////////////// vertical accordion /////////////////
+/////////////// vertical accordion /////////////////
 
+const team = document.querySelector('.team'),
+  teamAccoItem = document.querySelectorAll('.team-acco__item'),
+  teamAccoItemLength = teamAccoItem.length;
+
+team.addEventListener('click', e => {
+  for (let i = 0; i < teamAccoItemLength; i++) {
+    teamAccoItem[i].classList.remove('team-acco__item--active');
+  }
+});
+
+for (let i = 0; i < teamAccoItemLength; i++) {
+  teamAccoItem[i].addEventListener('click', e => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (teamAccoItem[i].classList.contains('team-acco__item--active')) {
+      teamAccoItem[i].classList.remove('team-acco__item--active');
+    }
+    else {
+      for (let i = 0; i < teamAccoItemLength; i++) {
+        teamAccoItem[i].classList.remove('team-acco__item--active');
+      }
+      teamAccoItem[i].classList.add('team-acco__item--active');
+    }
+  })
+}
+
+/////////////// modal window /////////////////
+
+const reviews = document.querySelector('.reviews'), 
+overlay = document.querySelector('.overlay'), 
+popupText = document.querySelector('.popup__text');
+
+reviews.addEventListener('click', e => {
+  let element = e.target;
+
+  if (element.tagName = 'BUTTON') {
+    let modalText = element.previousElementsibling.innerHTML
+
+    popupText.innerHTML = modalText; 
+    overlay.style.display = 'block';
+  }
+})
+
+document.addEventListener('keyup', e => {
+  let keyName = e.key;
+  if (keyName === 'Escape') {
+    overlay.style.dispLay = 'none';
+  }
+})
+
+/////////////// slider /////////////////
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const items = document.querySelector("#items");
+
+right.addEventListener('click', function (event) {
+  event.preventDefault();
+});
+
+right.addEventListener("click", function(e) {
+  loop("right", e);
+});
+ 
+left.addEventListener("click", function(e) {
+  loop("left", e);
+});
+
+function loop(direction, e) {
+  e.preventDefault();
+  if (direction === "right") {
+    items.appendChild(items.firstElementChild);
+
+  } else {
+    items.insertBefore(items.lastElementChild, 
+      items.firstElementChild);
+  }
+}
